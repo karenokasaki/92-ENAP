ec9abfac685d3a64a388ca10bf73300494e1679f
karen okasaki <oks.karen@gmail.com>
2022-12-05 14:46:34 -0300

    maps / renderizações condcionais

---
+++ week8/final-project/src/pages/HomePage.js
@@ -0,0 +1,70 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+function HomePage() {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [reload, setReload] = useState(true);
+
+  useEffect(() => {
+    async function fetchUsers() {
+      try {
+        const response = await axios.get("https://ironrest.cyclic.app/enap92");
+        setUsers(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchUsers();
+  }, [reload]);
+
+  async function handleDelete(id) {
+    try {
+      await axios.delete(`https://ironrest.cyclic.app/enap92/${id}`);
+      setReload(!reload);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  console.log(users);
+
+  return (
+    <div>
+      {!isLoading && (
+        <>
+          {users.map((user) => {
+            return (
+              <div key={user._id}>
+                <p>
+                  {user.nome} - {user.cargo} - {user.departamento} -{" "}
+                  <button onClick={() => handleDelete(user._id)}>
+                    exonerar
+                  </button>
+                </p>
+                <div>Tarefas:</div>
+                {user.tasks.map((task) => {
+                  return (
+                    <p
+                      key={task.task}
+                      style={{
+                        backgroundColor:
+                          task.status === "concluido" ? "green" : "blue",
+                      }}
+                    >
+                      {task.task} - {task.status}
+                    </p>
+                  );
+                })}
+              </div>
+            );
+          })}
+        </>
+      )}
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ week8/final-project/src/pages/ProfilePage.js
@@ -0,0 +1,11 @@
+
+
+function ProfilePage() {
+    return ( 
+        <div>
+            
+        </div>
+     );
+}
+
+export default ProfilePage;
